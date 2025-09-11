a8306e05983ce83a85fec8bec1e1a1e276f1fa30
AgilS121 <gilsatria121@gmail.com>
2025-09-11 11:46:09 +0700

    update routes

--- lib/hello_phoenix_web/router.ex
+++ lib/hello_phoenix_web/router.ex
@@ -15,22 +15,22 @@ defmodule HelloPhoenixWeb.Router do
     plug :accepts, ["json"]
   end
 
-  # scope "/", HelloPhoenixWeb do
-  #   pipe_through :browser
-
-  #   get "/", PageController, :home
-  #   resources "/tasks", TaskController
-
-  #   get "/hello", HelloController, :index
-
-  #   get "/_version", VersionController, :index
-
-  #   get "/health", HealthController, :check
-  # end
   scope "/", HelloPhoenixWeb do
-    pipe_through :api           # ← lebih ringan daripada :browser
+    pipe_through :browser
+
+    get "/", PageController, :home
+    resources "/tasks", TaskController
+
+    get "/hello", HelloController, :index
+
+    get "/_version", VersionController, :index
+
     get "/health", HealthController, :index
   end
+  # scope "/", HelloPhoenixWeb do
+  #   pipe_through :api           # ← lebih ringan daripada :browser
+  #   get "/health", HealthController, :index
+  # end
 
 
   # Other scopes may use custom stacks.
